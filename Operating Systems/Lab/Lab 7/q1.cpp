8a05d934a02f77cf52940043b4d507050d15bf74
Abdul Saboor <66201863+abdulsaboor938@users.noreply.github.com>
2022-10-20 18:57:02 +0500

    mac

--- Operating Systems/Lab/Lab 7/q1.cpp
+++ Operating Systems/Lab/Lab 7/q1.cpp
@@ -2,11 +2,35 @@
 // 20L-1113
 // BDS-5A1
 
+// program to explain working of file descriptors
+
 #include <iostream>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <fcntl.h>
 using namespace std;
 
 int main()
 {
+    int file_desc = open("dup.txt", O_WRONLY | O_APPEND);
+    if (file_desc < 0)
+        cout << "Error opening the file" << endl;
+    int pid = fork();
+    if (pid == 0)
+    {
+        // child process
+        int copy1_desc = dup(file_desc);
+        write(copy1_desc, "This will be output to the file named dup.txt\n", 46);
+        execlp("ls", "ls", "-l", NULL);
+        exit(0);
+    }
+    else
+    {
+        // parent process
+        cout << "Parent process" << endl;
+        int copy2_desc = dup(file_desc);
+        write(copy2_desc, "This will also be output to the file named dup.txt\n", 51);
+        wait(NULL);
+    }
+    return 0;
 }
--- Operating Systems/Lab/quiz1/l201113.cpp
+++ Operating Systems/Lab/quiz1/l201113.cpp
@@ -9,7 +9,8 @@ using namespace std;
 
 #define READ_END 0
 #define WRITE_END 1
-#define array_size 10
+#define array_size 1000
+#define array_size2 9
 
 int main()
 {
@@ -24,14 +25,19 @@ int main()
     {
         // child process
         close(pipefd[WRITE_END]);
-        int arr1[array_size];
+        int arr1[array_size][array_size2];
         read(pipefd[READ_END], arr1, sizeof(arr1));
 
         // calculating average
-        int avg = 0;
-        for (int i = 0; i < 10; i++)
+        long avg = 0;
+        for (int i = 0; i < array_size; i++)
         {
-            avg += arr1[i];
+            int avg1 = 0;
+            for (int j = 0; j < array_size2; j++)
+            {
+                avg1 += arr1[i][j];
+            }
+            avg += avg1 / array_size2;
         }
         cout << "Average of numbers is: " << avg / array_size << endl;
         close(pipefd[READ_END]);
@@ -41,11 +47,13 @@ int main()
     {
         // parent process
         close(pipefd[READ_END]);
-        int arr[array_size];
-        for (int i = 0; i < 10; i++)
+        int arr[array_size][array_size2];
+        for (int i = 0; i < array_size; i++)
         {
-            arr[i] = rand();
-            cout << arr[i] << " ";
+            for (int j = 0; j < array_size2; j++)
+            {
+                arr[i][j] = rand() % (array_size * array_size2);
+            }
         }
         cout << endl;
         write(pipefd[WRITE_END], arr, sizeof(arr));
